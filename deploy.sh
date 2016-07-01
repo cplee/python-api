8987f6ff039a172bdc942c456c7fd73c9c5def22
Casey Lee <cplee@nektos.com>
2016-06-30 23:20:02 -0700

    update dpeloy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 
 SHA1=$1
+IMAGE_NAME=324320755747.dkr.ecr.us-west-2.amazonaws.com/casey-test:$CIRCLE_SHA1
+
+# Push image
+eval `aws ecr get-login`
+docker tag python-api $TAG
+docker push $TAG
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=casey-labs-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+sed "s/<NAME>/$IMAGE_NAME/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name casey-test \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
